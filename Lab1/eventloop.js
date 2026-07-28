3f17fd161749782a8cc2db00c4f1f929fe3a563a
alok yadav <alok842959@gmail.com>
2026-07-28 13:48:38 +0530

    initial commit

---
+++ Lab1/eventloop.js
@@ -0,0 +1,21 @@
+const f1=()=>{
+f2();
+console.log("F1");    
+};
+const f2=()=>{
+f3();
+console.log("F2");    
+};
+const f3=()=>{
+f2();
+console.log("F3");    
+};
+const main=()=>{
+    console.log("main");
+    f1();
+    setTimeout(f2,5000);
+    f2();
+    f3();
+    console.log("end")
+};
+main();
