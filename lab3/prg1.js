c25f90d9c3e94fcdec9a46b0fcf35ac195441fa4
alok yadav <alok842959@gmail.com>
2026-09-01 14:51:16 +0530

    npm starts

---
+++ lab3/prg1.js
@@ -0,0 +1,14 @@
+import http from 'http';
+
+const server = http.createServer();
+
+server.on("request",(req,res) => {
+    res.write("Hello Client,I am your server");
+    res.end();
+});
+
+server.listen(3000, () => {
+    console.log("server is running on port 3000...")
+});
+ 
+
