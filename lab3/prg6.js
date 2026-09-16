b6d9b66217953ca86c48bedb95ae9ed8d1e86959
alok yadav <alok842959@gmail.com>
2026-09-16 12:22:36 +0530

    put and post

--- lab3/prg6.js
+++ lab3/prg6.js
@@ -19,9 +19,20 @@ const server = http.createServer((req, res) => {
         res.end(JSON.stringify({ message: "product added",product }));
         });
         
-    } else if (req.url === "/" && req.method === "PUT") {
-        res.statusCode = 200;
-        res.end("PUT Request");
+    } else if (req.url.startsWith("/products/") && req.method === "PUT") {
+        const productID = req.url.split("/").pop();
+        console.log("Upadate Product id:",productID);
+        let body = "";
+        req.on("data", (chunk) => {
+            body += chunk;
+        });
+        req.on("end", () => {
+            const product = JSON.parse(body);
+            console.log("received product:", product);
+            res.statusCode = 201;
+        res.end(JSON.stringify({ message: "product updated",product }));
+        });
+    
     } else if (req.url === "/" && req.method === "DELETE") {
         res.statusCode = 200;
         res.end("DELETE Request");
